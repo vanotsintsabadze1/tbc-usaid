ec4c9aaf1c36629e75727b084c8beafee7743658
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-01-25 03:07:26 +0400

    Minor addition to the burger menu / Fixed spelling and formatting issues in README

--- scripts/burgerMenu.js
+++ scripts/burgerMenu.js
@@ -1,9 +1,13 @@
 const wrapperMenu = document.querySelector(".burger_wrapper");
+const burgerLine = document.querySelectorAll(".burger_line");
 const navigationWrapper = document.querySelector(".navigation_wrapper");
 const navLinks = document.querySelectorAll(".nav_link");
 
 wrapperMenu.addEventListener("click", function () {
   wrapperMenu.classList.toggle("open");
+  burgerLine.forEach((burgerLine) => {
+    burgerLine.classList.toggle("burger_line_active");
+  });
   navigationWrapper.classList.toggle("navigation_wrapper_active");
 
   navLinks.forEach((navLink) => {
